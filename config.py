866f7997f6669ecc254f9520f380a0a645174d65
a-shimomura-shikoku-fuji <a-shimomura@shikoku-fuji.co.jp>
2026-05-26 08:45:28 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# データベース接続設定
+CONN_STR = r"DRIVER={ODBC Driver 17 for SQL Server};SERVER=FUJISV2\FUJIDEN;DATABASE=FUJIDENSEN;UID=sa;PWD=supervisor;"
+
+# Excelデザイン設定
+FONT_NAME = '游ゴシック'
+BG_HEADER = 'DCE6F1'  # ヘッダー：薄い青
+BG_TOTAL  = 'FCE4D6'  # 合計行：薄いベージュ
+
+# GUI共通設定
+GUI_BG_COLOR = "#f0f0f0"
+GUI_BTN_COLOR = "#0078d7"
